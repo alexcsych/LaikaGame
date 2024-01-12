71a856f74b9006fc5730b2ac3632242bd4675c3e
alexcsych <alexsejsi4@gmail.com>
2024-01-12 12:40:26 +0200

    Add fishki and amount

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import styles from './App.module.sass'
 
-function App() {
+function App () {
+  const limit = 200
+  const [money, setMoney] = useState(1000)
+  const [amount, setAmount] = useState(0)
+
+  const changeAmount = newAmount => {
+    if (limit >= newAmount + amount) {
+      setAmount(newAmount + amount)
+    }
+  }
+
+  const amounts = [1, 5, 25, 50, 100, 200]
+
+  const renderFishka = count => (
+    <div
+      key={count}
+      onClick={() => changeAmount(count)}
+      className={`${styles.fishka} ${styles[`fishka${count}`]} ${
+        limit < count + amount && styles.fishkaNot
+      }`}
+    ></div>
+  )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.body}>
+      <div className={styles.background}>
+        <img
+          src={'/images/background.jpg'}
+          alt='background'
+          className={styles.backgroundImg}
+        />
+        <div className={styles.amount1}>{limit}</div>
+        <div className={styles.amount2}>{money}</div>
+        <div className={styles.amount3}>{amount}</div>
+
+        <div>{amounts.map(amount => renderFishka(amount))}</div>
+
+        {amount > 0 && (
+          <>
+            <div onClick={() => setAmount(0)} className={styles.clearBtn}>
+              ОБНУЛИТЬ
+            </div>
+            <div className={styles.startBtn}>СДЕЛКА</div>
+          </>
+        )}
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
